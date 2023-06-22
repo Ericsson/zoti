696ebf777a3582d8cd951032e9efa0f047fe5a38
George Ungureanu <george.ungureanu@ericsson.com>
2023-06-22 11:00:57 +0200

    API restructuring. renamed module and CLI tool to 'zoml'.

--- zoti-yaml/src/zoml/__init__.py
+++ zoti-yaml/src/zoml/__init__.py
@@ -1,7 +1,6 @@
 from .core import INFO, POS, Pos, PosStack, Ref, attach_pos, get_pos
 from .exceptions import MarkedError, ModuleError, SearchError
 from .handlers import Module, Project
-from .loader import LoaderWithInfo
 
 from importlib.metadata import distribution
 __distribution__ = distribution("zoti_yaml")
